1461e5d0722ea996a1d37bf0986b1052400e783d
k-iizuka-mediaplex <k-iizuka@mediaplex.co.jp>
2026-05-26 15:33:17 +0900

    カテゴリ管理CRUD完了

--- database/migrations/2026_05_26_013300_create_categories_table.php
+++ database/migrations/2026_05_26_013300_create_categories_table.php
@@ -6,21 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
-    $table->id();
-    $table->string('name'); // カテゴリ名だけ
-    $table->timestamps();
-});
+            $table->id();
+            $table->string('name'); // カテゴリ名を保存する列
+            $table->timestamps();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('categories');
